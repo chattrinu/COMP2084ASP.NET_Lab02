8215df40aac599dd713255342c6ebd97e830431f
chattrinu <chattrinu@gmail.com>
2023-06-21 19:25:49 -0400

    Add project files.

---
+++ LabWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using LabWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+
+namespace LabWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Products> Products { get; set; }
+        
+    }
+}
---
+++ LabWebApp/Models/Products.cs
@@ -0,0 +1,11 @@
+﻿namespace LabWebApp.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
